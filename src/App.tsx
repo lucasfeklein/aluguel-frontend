71c91b7bf4bb963a814a9c19998fa6eaacd4999c
lucasfeklein <lucas.fklein@hotmail.com>
2023-11-02 18:48:46 +0800

    react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+function About() {
+  return <div>oi</div>;
+}
+
 function App() {
-  return <div className="text-red-500">oi</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
