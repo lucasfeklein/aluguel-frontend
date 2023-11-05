4d40ce32acd01035322e06d06cb9df1852ec7a6a
lucasfeklein <lucas.fklein@hotmail.com>
2023-11-05 10:52:49 +0800

    axios post request login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,35 @@
+import axios from "axios";
+import { useState } from "react";
+
 function Login() {
-  return <div>Login</div>;
+  const [pin, setPin] = useState("");
+
+  const handleLogin = async (e: any) => {
+    e.preventDefault();
+
+    const response = await axios.post("http://localhost:3000/auth", {
+      pin,
+    });
+  };
+  return (
+    <div className="flex justify-center mt-10">
+      <form onSubmit={handleLogin}>
+        <input
+          className="border border-gray-600"
+          type="text"
+          placeholder="Enter your PIN"
+          value={pin}
+          onChange={(e) => setPin(e.target.value)}
+        />
+        <button
+          className="bg-blue-400 text-white rounded-md px-4 py-2"
+          type="submit"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
