8844ebfb49157e9ebd67a23053aad19983444cb1
lucasfeklein <lucas.fklein@hotmail.com>
2023-11-10 15:40:44 +0800

    show delayed books

--- src/components/Books/DelayedBooks.tsx
+++ src/components/Books/DelayedBooks.tsx
@@ -1,6 +1,25 @@
 import { useEffect, useState } from "react";
 import api from "../../axiosConfig";
 
+type MonthDictionaryType = {
+  [key: string]: string;
+};
+
+const monthDictionary: MonthDictionaryType = {
+  1: "January",
+  2: "February",
+  3: "March",
+  4: "April",
+  5: "May",
+  6: "June",
+  7: "July",
+  8: "August",
+  9: "September",
+  10: "October",
+  11: "November",
+  12: "December",
+};
+
 type DelayedBooks = [string, { bookTitle: string; averageDelay: number }[]][];
 function DelayedBooks() {
   const [delayedBooks, setDelayedBooks] = useState<DelayedBooks | undefined>(
@@ -11,15 +30,30 @@ function DelayedBooks() {
   useEffect(() => {
     async function top3DelayedBooksPerMonth() {
       const response = await api("/book/top3delayedbooks");
-      console.log(Object.entries(response.data));
       setDelayedBooks(Object.entries(response.data));
     }
 
     top3DelayedBooksPerMonth();
   }, []);
+
+  console.log(delayedBooks);
   return (
     <div className="w-[90%] mx-auto mt-5">
-      Top 3 Delayed Books per Month in {currentYear}
+      <h2 className="text-lg font-bold">
+        Top 3 Delayed Books per Month in {currentYear}
+      </h2>
+      <div className="flex gap-4 flex-wrap">
+        {delayedBooks?.map(([month, books], idx) => (
+          <div key={idx} className="border w-[250px]">
+            <p className="font-bold">{monthDictionary[month.slice(5)]}</p>
+            <div>
+              {books.map((book) => (
+                <p>{book.bookTitle}</p>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
