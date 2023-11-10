8b569c408816e7ad68686cd2547d6c81ffa9f09c
lucasfeklein <lucas.fklein@hotmail.com>
2023-11-10 14:28:00 +0800

    books imports

---
+++ src/components/Books/DelayedBooks.tsx
@@ -0,0 +1,7 @@
+function DelayedBooks() {
+  return (
+    <div className="w-[90%] mx-auto mt-5">Top 3 Delayed Books per Month</div>
+  );
+}
+
+export default DelayedBooks;
--- src/components/Books/index.tsx
+++ src/components/Books/index.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import api from "../axiosConfig";
-import { useAuth } from "./AuthContext";
+import api from "../../axiosConfig";
+import { useAuth } from "../AuthContext";
+import DelayedBooks from "./DelayedBooks";
 
 type BookType = {
   id: string;
@@ -71,43 +72,46 @@ function Books() {
   }
 
   return (
-    <div className="flex flex-wrap justify-between gap-4 mt-10 w-[90%] mx-auto">
-      {books?.map((book) => (
-        <div
-          key={book.id}
-          className="border rounded-md flex w-[300px] p-4 flex-col items-center"
-        >
-          <h2 className="mb-4">{book.title}</h2>
-          {book.copies.map((copy, idx) => (
-            <div
-              key={idx}
-              className="flex w-[200px] justify-between mb-4 items-center"
-            >
-              <p>Copy {idx}</p>
-              {person ? (
-                <button
-                  style={{ backgroundColor: copy.isRented ? "gray" : "" }}
-                  className="bg-blue-500 text-white px-4 py-2 shadow-sm rounded-md"
-                  onClick={() => handleRent(copy.id, book.id)}
-                  disabled={copy.isRented}
-                >
-                  {copy.isRented && copy.rentedByPin === person.pin
-                    ? "You Rented"
-                    : copy.isRented
-                    ? "Already rented"
-                    : "Rent"}
-                </button>
-              ) : (
-                <Link to="/login">
-                  <button className="bg-blue-500 text-white px-4 py-2 shadow-sm rounded-md">
-                    Login to Rent
+    <div>
+      <div className="flex flex-wrap justify-between gap-4 mt-10 w-[90%] mx-auto">
+        {books?.map((book) => (
+          <div
+            key={book.id}
+            className="border rounded-md flex w-[300px] p-4 flex-col items-center"
+          >
+            <h2 className="mb-4">{book.title}</h2>
+            {book.copies.map((copy, idx) => (
+              <div
+                key={idx}
+                className="flex w-[200px] justify-between mb-4 items-center"
+              >
+                <p>Copy {idx}</p>
+                {person ? (
+                  <button
+                    style={{ backgroundColor: copy.isRented ? "gray" : "" }}
+                    className="bg-blue-500 text-white px-4 py-2 shadow-sm rounded-md"
+                    onClick={() => handleRent(copy.id, book.id)}
+                    disabled={copy.isRented}
+                  >
+                    {copy.isRented && copy.rentedByPin === person.pin
+                      ? "You Rented"
+                      : copy.isRented
+                      ? "Already rented"
+                      : "Rent"}
                   </button>
-                </Link>
-              )}
-            </div>
-          ))}
-        </div>
-      ))}
+                ) : (
+                  <Link to="/login">
+                    <button className="bg-blue-500 text-white px-4 py-2 shadow-sm rounded-md">
+                      Login to Rent
+                    </button>
+                  </Link>
+                )}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+      <DelayedBooks />
     </div>
   );
 }
