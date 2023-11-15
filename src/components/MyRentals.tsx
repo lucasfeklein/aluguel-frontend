d78a2b74bda38819f34a959cc1282086f3f08308
lucasfeklein <lucas.fklein@hotmail.com>
2023-11-15 15:50:40 +0800

    person not used in myrentals

--- src/components/MyRentals.tsx
+++ src/components/MyRentals.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../axiosConfig";
-import { useAuth } from "./AuthContext";
 
 type BookType = {
   id: string;
@@ -13,7 +12,6 @@ type BookType = {
 
 function MyRentals() {
   const navigate = useNavigate();
-  const { person } = useAuth();
 
   const [books, setBooks] = useState<BookType | undefined>(undefined);
 
