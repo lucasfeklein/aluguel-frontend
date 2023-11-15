3abce1cc614a5c5e47e5a62eea94ae62454d412a
lucasfeklein <lucas.fklein@hotmail.com>
2023-11-15 16:24:48 +0800

    a

--- src/axiosConfig.ts
+++ src/axiosConfig.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "https://busy-rose-dove-sari.cyclic.app" });
+const baseURL = import.meta.env.DEV
+  ? "http://localhost:3000"
+  : "https://busy-rose-dove-sari.cyclic.app";
+
+const api = axios.create({ baseURL });
 
 export default api;
