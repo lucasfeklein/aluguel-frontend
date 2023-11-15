cdc73b4c31ec9a862c12d5ef2a88fa4fb191e0ee
lucasfeklein <lucas.fklein@hotmail.com>
2023-11-15 16:16:42 +0800

    use localhost in dev and real backend in prod

--- src/axiosConfig.ts
+++ src/axiosConfig.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:3000" });
+const baseURL = import.meta.env.DEV
+  ? "http://localhost:3000"
+  : "https://busy-rose-dove-sari.cyclic.app";
+
+const api = axios.create({ baseURL });
 
 export default api;
